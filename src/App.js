0f4d103c9780888e86f9fae6df25810f33d40640
Nurzhigit <nurzigitturman@gmail.com>
2022-10-06 08:20:58 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
 import "./App.css";
-
+import { useState } from "react";
+import  calculateWinner  from './Components/Algoritm';
+import Square from './Components/Square';
 function App() {
-  const grid = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+  const [squares, setSquares] = useState(Array(9).fill(null))
+  const [isX, setIsX] = useState(true);
+
+  const handleClick = (i) => {
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
+
+    squares[i] = isX ? 'X' : 'O'
+    setSquares(squares)
+    setIsX(!isX)
+  }
+
+  const winner = calculateWinner(squares)
+  let status
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = 'Next player: ' + (isX ? 'X' : 'O');
+  }
+
+  const handleRestart = () => {
+    setIsX(true)
+    setSquares(Array(9).fill(null))
+  }
+
+  const renderSquare = (i) => {
+    return <Square value={squares[i]} onClick={() => handleClick(i)} />
+  }
+console.log(status)
 
   return (
-    <div className="game-board">
-      {grid.map((element) => (
-        <div key={element} className="box"></div>
-      ))}
-    </div>
+      <div className="game-board">
+          {squares.map((squares, i) => (
+              <div key={i} className="box">
+                {renderSquare(i)}
+              </div>
+          ))}
+        <div className="status">{status}</div>
+        <button className="restart" onClick={handleRestart}>Restart Game!</button>
+      </div>
   );
 }
 
---
+++ src/Components/Square.jsx
@@ -0,0 +1,10 @@
+import './Button.css';
+
+function Square({ onClick, value }) {
+    return (
+        <button className="square" onClick={onClick}>
+            {value}
+        </button>
+    );
+}
+export default Square;
